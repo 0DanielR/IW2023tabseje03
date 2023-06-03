e11b20aa6b7e368729fda0d2f8cef3c8afce3100
0DanielR <102000459+0DanielR@users.noreply.github.com>
2023-06-02 23:07:06 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IW2023tabseje03',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
